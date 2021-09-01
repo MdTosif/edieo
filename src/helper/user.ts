e2926597590ff3c7f0a4d8aa1910ae104f5c0f95
MDtosif <mdtosif0mt@gmail.com>
2021-09-01 14:10:50 +0530

    inititititt

--- src/helper/user.ts
+++ src/helper/user.ts
@@ -1,58 +1,78 @@
-import User from '../models/user';
-import { hash, compare } from 'bcryptjs';
-import { Sequelize } from 'sequelize';
+import User from "../models/user";
+import { hash, compare } from "bcryptjs";
+import { Sequelize } from "sequelize";
+import joi from "joi";
+
+const userValidation = joi.object({
+  username: joi
+    .string()
+    .trim()
+    .lowercase()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .insensitive()
+    .required(),
+  password: joi.string().min(6).max(50).required(),
+  email: joi.string().email().required(),
+});
 
 class UserHelper {
+  static async AddUser(data: {
+    username: string;
+    password: any;
+    email: string;
+  }) {
+    const userData = await userValidation.validateAsync(data);
+    const hashPassword = await hash(userData.password, 10);
+    userData.password = hashPassword;
+    const newUser = await User.create(userData);
+    console.log(newUser.toJSON());
+    return newUser.toJSON();
+  }
 
-    static async AddUser(data: { username: string; password: any; email: string; }) {
-        const hashPassword = await hash(data.password, 10);
-        data.password = hashPassword;
-        const newUser = await User.create(data);
-        console.log(newUser.toJSON());
-        return newUser.toJSON();
-    };
+  static async getAllUser() {
+    const allUser = await User.findAll({
+      attributes: ["username", "email"],
+    });
+    console.log(allUser);
+    return allUser;
+  }
 
-    static async getAllUser() {
-        const allUser = await User.findAll({
-            attributes: ['username', 'email'],
-        });
-        console.log(allUser);
-        return allUser;
-    }
+  static async deleteAllUser() {
+    const result = await User.destroy({
+      truncate: true,
+    });
+    console.log(result);
+    return result;
+  }
 
-    static async deleteAllUser() {
-        const result = await User.destroy({
-            truncate: true
-        });
-        console.log(result);
-        return result;
-    }
+  static async getUser(usernameOrEmail: string) {
+    const user = await User.findOne({
+      where: Sequelize.or(
+        { email: usernameOrEmail },
+        { username: usernameOrEmail }
+      ),
+    });
+    // console.log(user);
+    return user;
+  }
 
-    static async getUser(usernameOrEmail: string) {
-        const user = await User.findOne({
-            where: Sequelize.or(
-                { email: usernameOrEmail },
-                { username: usernameOrEmail })
-        });
-        // console.log(user);
-        return user;
-    }
+  static async authenticate(data: { user: string; password: any }) {
+    const user = await this.getUser(data.user);
+    const oldPassword = user?.getDataValue("password");
+    const result = await compare(data.password, oldPassword);
+    console.log(result);
 
-    static async authenticate(data: { user: string; password: any; }) {
-        const user = await this.getUser(data.user);
-        const oldPassword = user?.getDataValue('password');
-        const result = await compare(data.password, oldPassword);
-        console.log(result);
-
-        return result;
-    }
+    return result;
+  }
 }
 
-// UserHelper.AddUser({
-//     username: 'Tosif2',
-//     password: 'xy123',
-//     email: 'tosif1@xyoz.in'
-// });
+UserHelper.AddUser({
+  username: "Tosif2",
+  password: "xy12344",
+  email: "tosif1@gmao.in",
+});
 
 // UserHelper.getAllUser();
 // UserHelper.deleteAllUser();
@@ -63,7 +83,5 @@ class UserHelper {
 // });
 
 // UserHelper.getUser('Tosif4');
-
-
 
 export default UserHelper;
